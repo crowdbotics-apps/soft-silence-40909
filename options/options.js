d779fae7602be776f35d46356e9812bc36b97d3c
Rachel Teller <rachelteller1@gmail.com>
2023-05-22 20:24:24 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "soft-silence-40909",
-  url: "https://soft-silence-40909.botics.co",
-  api: "https://soft-silence-40909.botics.co/api/v1"
-}
+  "name": "soft-silence-40909",
+  "url": "https://soft-silence-40909.botics.co",
+  "api": "https://soft-silence-40909.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
